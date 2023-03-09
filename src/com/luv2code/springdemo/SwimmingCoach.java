20707fe10e91ff42bcc68182f2c43cdae3400583
huhuiwen <1050615501@qq.com>
2023-03-09 01:00:50 -0600

    di

--- src/com/luv2code/springdemo/SwimmingCoach.java
+++ src/com/luv2code/springdemo/SwimmingCoach.java
@@ -2,6 +2,29 @@ package com.luv2code.springdemo;
 
 public class SwimmingCoach implements Coach {
 	
+	private FortuneService fortuneService;
+	private String team;
+	
+	public SwimmingCoach() {
+		System.out.println("SwimmingCoach: inside no-arg constructor");
+		
+	}
+	
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("SwimmingCoach: inside setter method.");
+		this.fortuneService = fortuneService;
+	}
+	
+	
+	public String getTeam() {
+		return "Name: " + team;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("SwimmingCoach: inside setter method - setTeam.");
+		this.team = team;
+	}
+
 	@Override
 	public String getDailyWorkout() {
 		return "Swim for 10 cycles!";
@@ -10,8 +33,7 @@ public class SwimmingCoach implements Coach {
 	@Override
 	public String getDailyFortune() {
 		// TODO Auto-generated method stub
-		return "You'd be more careful today.";
-	}
-	
+		return fortuneService.getFortune();
+	}	
 
 }
