4b63b3a1388c0f120f6339342d00529a9471a279
huhuiwen <1050615501@qq.com>
2023-03-10 11:52:10 -0600

    modify annotation

--- spring-annotation/src/com/springdemo/TennisCoach.java
+++ spring-annotation/src/com/springdemo/TennisCoach.java
@@ -6,12 +6,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class TennisCoach implements Coach {
 	
+	@Autowired
 	private FortuneService fortuneService;
 	
-	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
-		fortuneService = theFortuneService;
-	}
+	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		fortuneService = theFortuneService;
+//	}
 
 	@Override
 	public String getDailyWorkout() {
