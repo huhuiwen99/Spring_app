5cbcb9f8f5192b0ef81e2db013238fb9a2f036cb
huhuiwen <1050615501@qq.com>
2023-03-10 01:14:41 -0600

    annotation

--- spring-annotation/src/com/springdemo/AnnotationDemoApp.java
+++ spring-annotation/src/com/springdemo/AnnotationDemoApp.java
@@ -13,10 +13,11 @@ public class AnnotationDemoApp {
 				new ClassPathXmlApplicationContext("applicationContext.xml");
 		
 		// get the bean from spring container
-		Coach theCoach = context.getBean("theSillyCoach", Coach.class);
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		
 		// call a method on the bean
 		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
 		
 		// close the context
 		context.close();
--- spring-annotation/src/com/springdemo/TennisCoach.java
+++ spring-annotation/src/com/springdemo/TennisCoach.java
@@ -1,14 +1,28 @@
 package com.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component("theSillyCoach")
+@Component
 public class TennisCoach implements Coach {
+	
+	private FortuneService fortuneService;
+	
+	@Autowired
+	public TennisCoach(FortuneService theFortuneService) {
+		fortuneService = theFortuneService;
+	}
 
 	@Override
 	public String getDailyWorkout() {
-		// TODO Auto-generated method stub
+
 		return "Practice your backhand volley!";
+	}
+	
+	@Override
+	public String getDailyFortune() {
+		// TODO Auto-generated method stub
+		return fortuneService.getFortune();
 	}
 
 }
